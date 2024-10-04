86453edd5b4c66cb1a78e11451b554201add9c73
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-10-04 23:47:34 +0100

    Trying to setup native

--- App.tsx
+++ App.tsx
@@ -76,10 +76,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
+          <Section title="Step One">Tharindu Senadeera</Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
