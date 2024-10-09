714cd55d0d6a307633c88c590fc8b4f9dd24fea0
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-10-09 14:10:31 +0100

    Banner image added / ttitle added

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -14,8 +14,20 @@ const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
+        <View style={{flex: 1, alignItems: 'center'}}>
           <Image source={require('../assets/logo.png')} style={styles.logo} />
+        </View>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Image source={require('../assets/man.png')} style={styles.banner} />
+        </View>
+        <View>
+          <Text style={styles.title}>Lorem ipsum dolor.</Text>
+        </View>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore 
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -32,5 +44,18 @@ const styles = StyleSheet.create({
   logo: {
     height: 40,
     width: 140,
+    marginVertical: 30,
+    marginHorizontal: 20,
+  },
+  banner: {
+    height: 240,
+    width: 230,
+    marginVertical: 20,
+  },
+  title: {
+    fontFamily: 'Poppins',
+    fontSize: 30,
+    fontWeight: 600,
+    textAlign: 'center',
   },
 });
