b88b7cdbc1915b513d28d21216b5b2990f43824e
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-10-15 08:32:20 +0100

    Welcome text and styles added

--- src/screen/LoginScreen.tsx
+++ src/screen/LoginScreen.tsx
@@ -11,6 +11,7 @@ import {
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {Colors} from '../utils/colors';
 import {useNavigation} from '@react-navigation/native';
+import {Fonts} from '../utils/fonts';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
@@ -20,9 +21,12 @@ const LoginScreen = () => {
   return (
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <TouchableOpacity onPress={handleRedirect}>
+        <TouchableOpacity onPress={handleRedirect} style={styles.backButton}>
           <AntDesign name="arrowleft" size={30} color={Colors.primary} />
         </TouchableOpacity>
+        <View style={styles.welcomeLayout}>
+          <Text style={styles.welcomeText}>Hey, Welcome Back</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -30,4 +34,20 @@ const LoginScreen = () => {
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backButton: {
+    marginVertical: 20,
+    marginHorizontal: 25,
+  },
+  welcomeLayout: {
+    height: 144,
+    width: 155,
+    marginHorizontal: 25,
+  },
+  welcomeText: {
+    fontFamily: Fonts.Medium,
+    fontSize: 32,
+    color: Colors.primary,
+    fontWeight: 600,
+  },
+});
